bc694aafdd1569cc9777f5c34d2621c8f7b6d015
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-03-01 12:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async function() {
+    const text = document.getElementById('text').value;
+    const delay = parseInt(document.getElementById('delay').value);
+
+    await delayFunction(delay);
+    
+    document.getElementById('output').innerText = text;
+  });
+
+  async function delayFunction(delay) {
+    return new Promise(resolve => {
+      setTimeout(resolve, delay);
+    });
+  }
